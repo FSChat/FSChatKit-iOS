b362f8d76fa65d4d0c623977cbe3361193a91aa2
Bassem Abbas <bassem.abbas@ibtikar.net.sa>
2020-07-01 17:04:42 +0200

    .gitignore fix

--- Pods/Target Support Files/Pods-FSChatKit-FSChatKitTests/Pods-FSChatKit-FSChatKitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FSChatKit_FSChatKitTests : NSObject
-@end
-@implementation PodsDummy_Pods_FSChatKit_FSChatKitTests
-@end
--- Pods/Target Support Files/Pods-FSChatKit/Pods-FSChatKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FSChatKit : NSObject
-@end
-@implementation PodsDummy_Pods_FSChatKit
-@end
